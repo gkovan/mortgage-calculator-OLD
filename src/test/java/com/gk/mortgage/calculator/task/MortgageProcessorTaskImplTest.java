8a2a25fa4a2de641ebcfd2fbc194507c969bc3a1
gkovan <NH43863@hertz.net>
2019-01-16 10:33:22 -0500

    cleanup the file

--- src/test/java/com/gk/mortgage/calculator/task/MortgageProcessorTaskImplTest.java
+++ src/test/java/com/gk/mortgage/calculator/task/MortgageProcessorTaskImplTest.java
@@ -1,37 +1,22 @@
 package com.gk.mortgage.calculator.task;
 
-import static org.hamcrest.CoreMatchers.any;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.gk.mortgage.calculator.domain.MortgageCalculatorRequest;
 import com.gk.mortgage.calculator.domain.MortgageCalculatorResponse;
-import com.gk.mortgage.calculator.service.InterestRateService;
 import com.gk.mortgage.calculator.service.InterestRateServiceImpl;
 import com.gk.mortgage.calculator.domain.interest.rate.InterestRatesResponse;
 import com.gk.mortgage.calculator.domain.interest.rate.InterestRate;
