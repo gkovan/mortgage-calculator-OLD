a71678a0e0e8d2f0b587fa530b1606d441095c2c
gkovan <NH43863@hertz.net>
2019-03-04 18:01:38 -0500

    march 4 TDD  and SOLID paper

---
+++ src/main/java/com/gk/mortgage/calculator/controller/MortgageCalculatorController_NoDependencyInjection.java
@@ -0,0 +1,26 @@
+package com.gk.mortgage.calculator.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gk.mortgage.calculator.domain.MortgageCalculatorRequest;
+import com.gk.mortgage.calculator.domain.MortgageCalculatorResponse;
+import com.gk.mortgage.calculator.task.MortgageProcessorTask;
+import com.gk.mortgage.calculator.task.MortgageProcessorTaskImpl;
+import com.gk.mortgage.calculator.task.ValidateInputTask;
+import com.gk.mortgage.calculator.task.ValidateInputTaskImpl;
+
+@RestController
+public class MortgageCalculatorController_NoDependencyInjection {
+		
+	@PostMapping("/calculate-no-dependency-injection")
+	public MortgageCalculatorResponse calculateMonthlyPayment(@RequestBody MortgageCalculatorRequest request) {		
+		
+		   MortgageProcessorTask mortgageProcessorTask = new MortgageProcessorTaskImpl();
+		   ValidateInputTask validateInputTask = new ValidateInputTaskImpl();	
+		   
+		   validateInputTask.validate(request);
+		   return mortgageProcessorTask.process(request);
+	}
+}
