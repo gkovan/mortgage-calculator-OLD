6b3b4ae14cf34f809e224aa35bba4b34d91ef8c3
gkovan <gkovan@hotmail.com>
2019-07-14 16:49:03 -0400

    Revert "Logging aspect"

--- src/main/java/com/gk/mortgage/calculator/annotations/Password.java
+++
@@ -1,8 +0,0 @@
-package com.gk.mortgage.calculator.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Password {
-}
--- src/main/java/com/gk/mortgage/calculator/domain/MortgageCalculatorRequest.java
+++ src/main/java/com/gk/mortgage/calculator/domain/MortgageCalculatorRequest.java
@@ -1,27 +1,15 @@
 package com.gk.mortgage.calculator.domain;
 
-import com.gk.mortgage.calculator.annotations.PCI;
-import com.gk.mortgage.calculator.annotations.PII;
-import com.gk.mortgage.calculator.annotations.Mask;
-
+import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Mask
+@Builder(toBuilder = true)
 public class MortgageCalculatorRequest {
 	
-	@PII
-	String name;
-	@PII
-	String propertyAddress;
-	@PCI(keepLastDigits=4)
-	String creditCard;
-	@PCI
-	String creditCardExpiry;
-	@PII(keepLastDigits=4)
-	String socialSecurityNumber;
-	Double principal;
-	Double interestRate;
-	Integer term;
-	String type;
+	final Double principal;
+	final Double interestRate;
+	final Integer term;
+	final String type;
+	final String amortization;
 }
