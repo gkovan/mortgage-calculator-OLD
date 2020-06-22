ef02d2844eedbae86956f2f5b5d63173097294aa
gkovan <NH43863@hertz.net>
2020-06-22 13:12:42 -0400

    basic error handling working

--- src/main/java/com/gk/mortgage/calculator/service/InterestRateServiceImpl.java
+++ src/main/java/com/gk/mortgage/calculator/service/InterestRateServiceImpl.java
@@ -3,9 +3,12 @@ package com.gk.mortgage.calculator.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import com.gk.mortgage.calculator.domain.Messages;
 import com.gk.mortgage.calculator.domain.interest.rate.InterestRatesResponse;
+import com.gk.mortgage.calculator.exceptions.InterestRateServiceException;
 
 import lombok.Setter;
 
@@ -16,17 +19,25 @@ public class InterestRateServiceImpl implements InterestRateService {
     @Value("${environment.interestrate.uri}")
 	private String interestRateUri;
 	
+	private Messages messages;
+	
 	private RestTemplate restTemplate;
 
 
 	@Autowired
-    public InterestRateServiceImpl(RestTemplate restTemplate) {
+    public InterestRateServiceImpl(RestTemplate restTemplate, Messages messages) {
 		super();
 		this.restTemplate = restTemplate;
+		this.messages = messages;
 	}
 	
 	@Override
 	public InterestRatesResponse getRates() {
-		return restTemplate.getForObject(interestRateUri, InterestRatesResponse.class);
+		try {
+		InterestRatesResponse interestRatesResponse = restTemplate.getForObject(interestRateUri, InterestRatesResponse.class);
+		return interestRatesResponse;
+		} catch (RestClientException e) {
+			throw new InterestRateServiceException("MC0003", messages.getProperty("error.MC0003.message"));
+		}
 	}
 }
